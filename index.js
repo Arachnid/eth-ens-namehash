1dea90051b2dc17823171d3d2b2f5bea9e33bb79
Dan Finlay <dan@danfinlay.com>
2017-11-22 19:25:45 -0800

    Do not use template strings for es5 compat

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ function namehash (inputName) {
     }
   }
 
-  return `0x${node}`
-}
+  return '0x' + node
 
 function normalize(name) {
   return name ? uts46.toUnicode(name, {useStd3ASCII: true, transitional: false}) : name
