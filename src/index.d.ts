5a8272f50c8d09c0e2c0892450f32825da752f45
Andrew Raffensperger <raffy@me.com>
2022-09-25 15:00:49 -0700

    forgot optional nf argument

--- src/index.d.ts
+++ src/index.d.ts
@@ -38,15 +38,16 @@ interface StopToken {
 type Token = TextToken | EmojiToken | NFCToken | StopToken;
 
 export function hash(name: string): string;
-export function normhash(name: string): string;
+export function namehash(name: string): string;
 export function labelhash(label: string): string;
+export function normhash(name: string): string;
 
 export function normalize(name: string): string;
 export function beautify(name: string): string;
 export function tokenize(name: string): Token[];
 export function supportedEmojis(): number[][];
 
-export function normalizeFragment(frag: string): string;
+export function normalizeFragment(frag: string, nf?: (cps: number[]) => number[]): string;
 export function normalizePostCheck(norm: string): string;
 
 export function nfd(cps: number[]): number[];
